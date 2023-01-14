332b0d3f752ba6599a7cee7d26f8ddde48d21489
ahmad-odeh21 <ahmad_ode21@outlook.com>
2023-01-14 19:26:03 +0300

    first commit

---
+++ src/main/java/com/dentist/app/exception/InvalidAppointmentDate.java
@@ -0,0 +1,8 @@
+package com.dentist.app.exception;
+
+public class InvalidAppointmentDate extends RuntimeException {
+    public InvalidAppointmentDate(String message) {
+        super(message);
+    }
+
+}
